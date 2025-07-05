e888ba05a78dbc84c8caa63b2d1c8556a3fa9570
SilentBlades <tripathysamirkumar9427@gmail.com>
2025-07-05 09:30:30 +0530

    -Add validations -Add unit test: FieldParserTest

--- src/main/java/com/deliverooassignment/cron/FieldParser.java
+++ src/main/java/com/deliverooassignment/cron/FieldParser.java
@@ -3,38 +3,103 @@ package com.deliverooassignment.cron;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
+/**
+ * This method parses each segment of the cron. Each segment of the cron could be:
+ * <ol>
+ *  <li>Limits: 1,2,3,4 or 1 or *</li>
+ *  <li>Wildcard: *</li>
+ *  <li>Range: start-end</li>
+ *  <li>Step: &#42/n or start-end/n</li>
+ * </ol>
+ *
+ * <ul>
+ *   <li><b>Limits:</b>
+ *     <li>Add all the numbers in the list to the result</li>
+ *     <li>Validation: The numbers should be in the valid range(inclusive) and not empty</li>
+ *   </li>
+ *
+ *   <li><b>Wildcard:</b>
+ *     <li>Add all numbers between min(inclusive) and max(inclusive)</li>
+ *   </li>
+ *
+ *   <li><b>Range:</b>
+ *     <li>Add all numbers between start(inclusive)-end(inclusive)</li>
+ *     <li>Validation: start < end; start >=min and end <= max and start and end should not be empty</></li>
+ *   </li>
+ *
+ *   <li><b>Step:</b>
+ *     <li>Add all numbers by skipping n numbers. * denotes all numbers between min and max. start-end denotes a range</li>
+ *     <li>Validation: n cannot be <= 0; All validations for range also apply here</li>
+ *   </li>
+ * </ul>
+ */
+
 public class FieldParser {
     public static SortedSet<Integer> parse(String spec, int min, int max) {
         SortedSet<Integer> result = new TreeSet<>();
 
-        for (String part : spec.split(",")) {
-            if (part.equals("*")) {
+        // 1. limits
+        for (String segment : spec.split(",")) {
+            // 2. Wildcard
+            if (segment.equals("*")) {
                 for (int i = min; i <= max; i++) {
                     result.add(i);
                 }
-            } else if (part.contains("/")) {
-                String[] split = part.split("/");
-                String range = split[0];
-                int step = Integer.parseInt(split[1]);
+            }
+            // 3. Step
+            else if (segment.contains("/")) {
+                String[] split = segment.split("/");
+                if(split.length != 2 || split[0].isEmpty() || split[1].isEmpty()) {
+                    throw new IllegalArgumentException("Invalid step specified: " + segment);
+                }
 
+                String range = split[0];
                 int start = min, end = max;
+                int step = Integer.parseInt(split[1]);
+                if(step <= 0) {
+                    throw new IllegalArgumentException("Step <= 0: " + segment);
+                }
+
                 if (!range.equals("*")) {
                     String[] bounds = range.split("-");
+                    if(bounds.length != 2 || bounds[0].isEmpty() || bounds[1].isEmpty()) {
+                        throw new IllegalArgumentException("Invalid range in step: " + segment);
+                    }
                     start = Integer.parseInt(bounds[0]);
                     end   = Integer.parseInt(bounds[1]);
+                    if(start > end) {
+                        throw new IllegalArgumentException("Incorrect range specified: " + segment);
+                    }
+                    if(start < min || end > max) {
+                        throw new IllegalArgumentException("Segment is out of bounds: " + segment);
+                    }
                 }
                 for (int i = start; i <= end; i += step) {
                     result.add(i);
                 }
-            } else if (part.contains("-")) {
-                String[] bounds = part.split("-");
+            }
+            // 4. Range
+            else if (segment.contains("-")) {
+                String[] bounds = segment.split("-");
+                if(bounds.length != 2 || bounds[0].isEmpty() || bounds[1].isEmpty()) {
+                    throw new IllegalArgumentException("Invalid range in step: " + segment);
+                }
                 int start = Integer.parseInt(bounds[0]);
                 int end   = Integer.parseInt(bounds[1]);
+                if(start > end) {
+                    throw new IllegalArgumentException("Incorrect range specified: " + segment);
+                }
+                if(start < min || end > max) {
+                    throw new IllegalArgumentException("Segment is out of bounds: " + segment);
+                }
                 for (int i = start; i <= end; i++) {
                     result.add(i);
                 }
             } else {
-                result.add(Integer.parseInt(part));
+                if(Integer.parseInt(segment) < min || Integer.parseInt(segment) > max) {
+                    throw new IllegalArgumentException("Segment is out of bounds: " + segment);
+                }
+                result.add(Integer.parseInt(segment));
             }
         }
 
--- src/test/java/com/deliverooassignment/cron/FieldParserTest.java
+++ src/test/java/com/deliverooassignment/cron/FieldParserTest.java
@@ -1,4 +1,133 @@
 package com.deliverooassignment.cron;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class FieldParserTest {
+    final int min = 0;
+    final int max = 12;
+
+    // Happy Paths
+    // 1. Full Range
+    @Test
+    void fullRangeTest() {
+        SortedSet<Integer> input = FieldParser.parse("*", min, 3);
+        SortedSet<Integer> output = new TreeSet<>(Arrays.asList(0, 1, 2, 3));
+        assertEquals(input, output);
+    }
+
+    // 2. Simple Range
+    @Test
+    void simpleRangeTest() {
+        SortedSet<Integer> input = FieldParser.parse("3-6", min, max);
+        SortedSet<Integer> output = new TreeSet<>(Arrays.asList(3, 4, 5, 6));
+        assertEquals(input, output);
+    }
+
+    // 3. Step
+    @Test
+    void stepTest() {
+        SortedSet<Integer> input1 = FieldParser.parse("*/3", min, max);
+        SortedSet<Integer> output1 = new TreeSet<>(Arrays.asList(0, 3, 6, 9, 12));
+        assertEquals(input1, output1);
+
+        SortedSet<Integer> input2 = FieldParser.parse("2-10/3", min, max);
+        SortedSet<Integer> output2 = new TreeSet<>(Arrays.asList(2, 5, 8));
+        assertEquals(input2, output2);
+
+        SortedSet<Integer> input3 = FieldParser.parse("2-10/12", min, max);
+        SortedSet<Integer> output3 = new TreeSet<>(Arrays.asList(2));
+        assertEquals(input3, output3);
+    }
+
+    // 4. List
+    @Test
+    void listTest() {
+        SortedSet<Integer> input = FieldParser.parse("1,8,11", min, max);
+        SortedSet<Integer> output = new TreeSet<>(Arrays.asList(1, 8, 11));
+        assertEquals(input, output);
+    }
+
+    // 5. Duplicate
+    @Test
+    void duplicateTest() {
+        SortedSet<Integer> actual = FieldParser.parse("*/5,1-10,6,8", min, max);
+        SortedSet<Integer> expected = new TreeSet<>(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+        assertEquals(expected, actual);
+    }
+
+    // Sad Paths
+    // 1. NonNumeric input
+    @Test
+    void nonNumericInputTest() {
+        assertThrows(NumberFormatException.class, () -> FieldParser.parse("a-b", min, max));
+    }
+
+    // 2. Reversed range
+    @Test
+    void reversedRangeTest() {
+        assertThrows(IllegalArgumentException.class,
+                () -> FieldParser.parse("10-5/5", min, max));
+    }
+
+    // 3. 0 step
+    @Test
+    void zeroStepTest() {
+        assertThrows(IllegalArgumentException.class,
+                () -> FieldParser.parse("*/0", min, max));
+    }
+
+    // 4. Out of boundaries
+    @Test
+    void outOfBoundariesTest() {
+        assertThrows(IllegalArgumentException.class,
+                () -> FieldParser.parse("13", min, max));
+
+        assertThrows(IllegalArgumentException.class,
+                () -> FieldParser.parse("1", 2, max));
+
+        assertThrows(IllegalArgumentException.class,
+                () -> FieldParser.parse("1-13", min, max));
+
+        assertThrows(IllegalArgumentException.class,
+                () -> FieldParser.parse("1-12", 2, max));
+    }
+
+    // 5. Empty segment
+    @Test
+    void emptySegmentTest() {
+        assertThrows(IllegalArgumentException.class,
+                () -> FieldParser.parse("", min, max));
+
+        assertThrows(IllegalArgumentException.class,
+                () -> FieldParser.parse("-13", min, max));
+
+        assertThrows(IllegalArgumentException.class,
+                () -> FieldParser.parse("1-", min, max));
+
+        assertThrows(IllegalArgumentException.class,
+                () -> FieldParser.parse("-10/3", min, max));
+
+        assertThrows(IllegalArgumentException.class,
+                () -> FieldParser.parse("1-/3", min, max));
+    }
+
+    // 6. Space within segment
+    @Test
+    void spaceWithinSegmentTest() {
+        assertThrows(NumberFormatException.class,
+                () -> FieldParser.parse("1, 2, 3", min, max));
+
+        assertThrows(NumberFormatException.class,
+                () -> FieldParser.parse("1 - 12", min, max));
+
+        assertThrows(NumberFormatException.class,
+                () -> FieldParser.parse("* / 2", min, max));
+    }
 }
